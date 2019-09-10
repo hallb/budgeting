2798f9ab51ce3d45cda63f88047fb844a4cbcba9
Ben Hall <ben.hall@intelliware.ca>
2019-09-10 15:55:17 -0400

    Completed rewrite as Iterators.

--- budgeting.py
+++ budgeting.py
@@ -1,10 +1,13 @@
 import reactive_dates
 from reactive_dates import DateRange
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Iterator, TypeVar, Callable, Optional
+from functools import partial
 from decimal import Decimal
 from datetime import date
 
-Money = Decimal  # TODO...really should be decimal
+
+# Domain definitions
+Money = Decimal
 
 
 class BudgetSpec(NamedTuple):
@@ -24,25 +27,54 @@ class SummaryTransaction(NamedTuple):
     balance: Money
 
 
-def gen_budget(budget_specs: List[BudgetSpec], from_to: DateRange) -> List[Transaction]:
-    result: List[Transaction] = []
-    for s in budget_specs:
-        event_dates = filter(s.date_predicate, reactive_dates.daily(from_to))
-        result.extend([Transaction(s.name, s.amount, ed) for ed in event_dates])
-    return result
+def budget_specified_transaction_on_day(spec: BudgetSpec, day: date) -> Optional[Transaction]:
+    return Transaction(spec.name, spec.amount, day) if spec.date_predicate(day) else None
 
 
-def summarize_transactions(transactions: List[Transaction]) -> List[SummaryTransaction]:
-    result: List[SummaryTransaction] = []
-    running_total = Money('0.00')
-    for transaction in transactions:
-        running_total = running_total + transaction.amount
-        summary_transaction = SummaryTransaction(transaction, running_total)
-        result.append(summary_transaction)
-    return result
+def transactions_for_budget_on_day(budget_specs: List[BudgetSpec], day: date) -> Iterator[Transaction]:
+    for spec in budget_specs:
+        transaction = budget_specified_transaction_on_day(spec, day)
+        if transaction is not None:
+            yield transaction
 
 
-def generate_budget(budget_specs: List[BudgetSpec], date_range: DateRange) -> List[SummaryTransaction]:
-    transactions: List[Transaction] = gen_budget(budget_specs, date_range)
-    sorted_transactions: List[Transaction] = sorted(transactions, key=lambda tx: tx.date)
-    return summarize_transactions(sorted_transactions)
+def increment_summary_transaction(previous: SummaryTransaction, current: Transaction) -> SummaryTransaction:
+    previous_balance = Money('0.00') if previous is None else previous.balance
+    return SummaryTransaction(current, previous_balance + current.amount)
+
+
+def project_budget(budget_specs: List[BudgetSpec], date_range: DateRange) -> List[SummaryTransaction]:
+    return list(project_budget_iterator(budget_specs, date_range))
+
+
+def project_budget_iterator(budget_specs, date_range):
+    # Curry the budget_specs into the Transactions generator
+    transactions_for_day = partial(transactions_for_budget_on_day, budget_specs)
+    # Step 1. What are the dates?
+    dates = reactive_dates.daily(date_range)
+    # Step 2. What are the transactions for those dates?
+    transactions = flat_map_iterator(dates, transactions_for_day)
+    # Step 3. What's the running balance?
+    z: SummaryTransaction = None  # Needed a type hint.
+    summary_transactions = scan_left_iterator(transactions, z, increment_summary_transaction)
+    return summary_transactions
+
+
+# TODO: move to a utility module
+A = TypeVar('A')
+B = TypeVar('B')
+
+
+# TODO: move to a utility module
+def flat_map_iterator(it: Iterator[A], f: Callable[[A], Iterator[B]]) -> Iterator[B]:
+    for i in it:
+        for j in f(i):
+            yield j
+
+
+# TODO: move to a utility module
+def scan_left_iterator(it: Iterator[A], z: B, f: Callable[[B, A], B]) -> Iterator[B]:
+    previous = z
+    for i in it:
+        previous = f(previous, i)
+        yield previous
--- budgeting_test.py
+++ budgeting_test.py
@@ -1,21 +1,15 @@
 import unittest
-from datetime import date as d
-
-from budgeting import BudgetSpec, gen_budget, generate_budget, SummaryTransaction, Transaction, Money
-import reactive_dates
+from datetime import date as d, timedelta as td
+from budgeting import *
 
 
 class TestSavingsAndBudget(unittest.TestCase):
 
-    def test_gen_budget(self):
-        budget_spec_1 = BudgetSpec("apple", Money('123.45'), reactive_dates.onL(d(2018, 1, 1)))
-        budget_spec_2 = BudgetSpec("banana", Money('234.45'), reactive_dates.onL(d(2018, 1, 2)))
-        budget_specs = [budget_spec_1, budget_spec_2]
-        actual = gen_budget(budget_specs, (d(2017, 8, 15), d(2018, 8, 15)))
-
-        self.assertEqual(2, len(actual))
-        self.assert_equal_budget_specs(budget_spec_1, actual[0])
-        self.assert_equal_budget_specs(budget_spec_2, actual[1])
+    def test_budget_specified_transaction_on_day(self):
+        a_date = d(2018, 1, 1)
+        budget_spec = BudgetSpec("apple", Money('123.45'), reactive_dates.onL(a_date))
+        self.assertTrue(budget_specified_transaction_on_day(budget_spec, a_date))
+        self.assertFalse(budget_specified_transaction_on_day(budget_spec, a_date + td(1)))
 
     def test_generate_budget(self):
         from_to = (d(2017, 11, 23), d(2018, 11, 23))
@@ -24,15 +18,10 @@ class TestSavingsAndBudget(unittest.TestCase):
             BudgetSpec('Opening Balance', Money('123.45'), reactive_dates.onL(d(2017, 11, 23))),
             BudgetSpec('Hydro Offset', Money('67.89'), reactive_dates.onL(d(2017, 11, 24)))
         ]
-        actual = generate_budget(budget_specs, from_to)
+        actual = project_budget(budget_specs, from_to)
         expected = [SummaryTransaction(Transaction("Opening Balance", Money('123.45'), d(2017, 11, 23)), Money('123.45')),
                     SummaryTransaction(Transaction("Hydro Offset", Money('67.89'), d(2017, 11, 24)), Money('191.34'))]
         self.assertEqual(expected, actual)
-
-    def assert_equal_budget_specs(self, expected, actual):
-        self.assertEqual(expected.name, actual.name)
-        self.assertEqual(expected.amount, actual.amount)
-        self.assertTrue(expected.date_predicate(actual.date))
 
 
 if __name__ == '__main__':
